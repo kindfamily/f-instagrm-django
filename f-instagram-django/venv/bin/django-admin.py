fb682b32e06d3b80ea57031393e8fe0c5cb24ea1
bill <bill@naver.com>
2019-12-04 23:01:42 +0900

    new: f-instagram 소스를 clone받아서 f-instagram-django 저장소에 장고프로젝트를 만들어라

---
+++ f-instagram-django/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/tuntunkimpo/Desktop/instagrom/03_back-end-public/f-instagram-django/venv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
